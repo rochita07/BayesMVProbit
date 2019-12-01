761a99e0735cb99b0bfad45710b1af1ba2e2a65c
Rochita <rochita.das.stat@gmail.com>
2019-12-01 02:36:25 -0600

    Creation of z_matrix (Utility score matrix)

--- R/Multivariate Nominal.r
+++ R/Multivariate Nominal.r
@@ -15,9 +15,7 @@ set.seed(1287)
 beta_act = matrix(rep(2, beta_dim), nrow = z_dim)
 beta_act
 x_mat = matrix(rnorm(n * k), nrow = k ) # each column is for each subject, each row is for each  covariates
-z_mat = beta_act %*% x_mat
 dim(x_mat)
-dim(z_mat)
 
 # A specific choice of sigma matrix (mentioned in the example of paper)
 sig1 = matrix(c(1, 0.36, 0.36, 0.81), nrow = 2)
@@ -25,4 +23,14 @@ sig2 = matrix(c(1, 0.45, 0.24, 0.45, 0.81, 0.41, 0.24, 0.41, 0.9), nrow =3)
 sig3 = matrix(c(rep(0.2, 6)), nrow =2)
 sig4 = matrix(c(rep(0.2, 6)), nrow =3)
 sig_gen = as.matrix(rbind(cbind(sig1, sig3), cbind(sig4, sig2)))
-sig_gen  ## Final sigma matrix for e
+sig_gen  ## Final sigma matrix for error
+
+e_mat = MASS::mvrnorm(n , mu = rep(0, z_dim) , Sigma = sig_gen )  # generation of error 
+e_mat = t(e_mat)   # each column is for each subject, each row is for each  covariates
+dim(e_mat)
+
+z_mat = matrix(rep(0, n * z_dim), ncol = n)   # each column is for each subject, each row is for each  covariates
+dim(z_mat)
+
+z_mat = beta_act %*% x_mat + e_mat
+  
