6ce5822145d6362199612d3804a5f26fad31c8b2
Rochita <rochita.das.stat@gmail.com>
2019-12-02 05:51:47 -0600

    Updates of delta

--- R/ordinal.R
+++ R/ordinal.R
@@ -47,37 +47,122 @@ beta_update_var = solve(prior_beta_var + tcrossprod(x))
 beta_mat = matrix(rep(0, k * iter), nrow = iter)
 
 
-# calculation of likelihood                        
-for(i in 1: n)
+## updates of delta
+
+f = 1 ## initialization
+
+y_given_beta_delta_optim = function(delta)
 {
-  if(y[i] == 1)
-  {
-    a = (-t(x[,i]) %*% beta) / sig
-    b = -Inf
-    f = f * ( pnorm(a) - pnorm(b))   # pnorm(-inf) = 0 and nu1 = 0
-  }
   
-  if(y[i] == 2)
-  {
-    a = ( exp(delta[1]) - (t(x[,i]) %*% beta) )/ sig
-    b = (-t(x[,i]) %*% beta) / sig
-    f = f * ( pnorm( a )  - pnorm(b) )
-  }
+  prior_delta = mvtnorm::dmvnorm(delta, prior_delta_mean, prior_delta_var )  # prior density for delta
   
-  for( j in 3 : (cat-1))
+  # calculation of likelihood                        
+  for(i in 1: n)
   {
-    if(y[i] == j)
+    if(y[i] == 1)
     {
-      a = (sum(exp(delta[1:(j-1)])) -  (t(x[,i]) %*% beta) ) / sig
-      b = (sum(exp(delta[1:(j-2)])) -  (t(x[,i]) %*% beta) ) / sig
-      f = f * ( pnorm(a)  - pnorm(b) )
+      a = (-t(x[,i]) %*% beta) / sig
+      b = -Inf
+      f = f * ( pnorm(a) - pnorm(b))   # pnorm(-inf) = 0 and nu1 = 0
     }
+    
+    if(y[i] == 2)
+    {
+      a = ( exp(delta[1]) - (t(x[,i]) %*% beta) )/ sig
+      b = (-t(x[,i]) %*% beta) / sig
+      f = f * ( pnorm( a )  - pnorm(b) )
+    }
+    
+    for( j in 3 : (cat-1))
+    {
+      if(y[i] == j)
+      {
+        a = (sum(exp(delta[1:(j-1)])) -  (t(x[,i]) %*% beta) ) / sig
+        b = (sum(exp(delta[1:(j-2)])) -  (t(x[,i]) %*% beta) ) / sig
+        f = f * ( pnorm(a)  - pnorm(b) )
+      }
+    }
+    if(y[i] == cat)
+    {
+      a = Inf
+      b = (sum(exp(delta)) - (t(x[,i]) %*% beta) ) / sig
+      f = f * ( pnorm( a )  - pnorm(b) )
+    }
+    
   }
-  if(y[i] == cat)
-  {
-    a = Inf
-    b = (sum(exp(delta)) - (t(x[,i]) %*% beta) ) / sig
-    f = f * ( pnorm( a )  - pnorm(b) )
-  }
+  optim_fn = - f * prior_delta
+  return(optim_fn)
   
 }
+
+delta_hat = optim(par = rep(0, nu), fn =  y_given_beta_delta_optim)$par
+delta_hat
+
+f = 1
+y_given_beta_delta_hessian = function(delta)
+{
+  prior_delta = mvtnorm::dmvnorm(delta, prior_delta_mean, prior_delta_var )
+  
+  # calculation of likelihood                        
+  for(i in 1: n)
+  {
+    if(y[i] == 1)
+    {
+      a = (-t(x[,i]) %*% beta) / sig
+      b = -Inf
+      f = f * ( pnorm(a) - pnorm(b))   # pnorm(-inf) = 0 and nu1 = 0
+    }
+    
+    if(y[i] == 2)
+    {
+      a = ( exp(delta[1]) - (t(x[,i]) %*% beta) )/ sig
+      b = (-t(x[,i]) %*% beta) / sig
+      f = f * ( pnorm( a )  - pnorm(b) )
+    }
+    
+    for( j in 3 : (cat-1))
+    {
+      if(y[i] == j)
+      {
+        a = (sum(exp(delta[1:(j-1)])) -  (t(x[,i]) %*% beta) ) / sig
+        b = (sum(exp(delta[1:(j-2)])) -  (t(x[,i]) %*% beta) ) / sig
+        f = f * ( pnorm(a)  - pnorm(b) )
+      }
+    }
+    if(y[i] == cat)
+    {
+      a = Inf
+      b = (sum(exp(delta)) - (t(x[,i]) %*% beta) ) / sig
+      f = f * ( pnorm( a )  - pnorm(b) )
+    }
+    
+  }
+  
+  hessian_fn = log(f * prior_delta)
+  return(hessian_fn)
+}
+hessian_mat = numDeriv::hessian(func = y_given_beta_delta_hessian , x = delta_hat)
+D_hat = solve(-hessian_mat)
+D_hat  # var- cov matrix , to be converted into scale matrix
+
+scale_t = D_hat * ((df_t-2) / df_t)  # scale matrix for poposal t distn
+
+# delta_current = mvtnorm::rmvt(n = 1, delta = delta_hat , sigma = scale_t, df = df_t)  
+delta_proposed = mvtnorm::rmvt(n = 1, delta = delta_hat , sigma = scale_t, df = df_t)
+density_delta_current = mvtnorm::dmvt(delta_current, delta = delta_hat , sigma = scale_t, df = df_t, log = FALSE )
+density_delta_proposed = mvtnorm::dmvt(delta_proposed, delta = delta_hat , sigma = scale_t, df = df_t, log = FALSE )
+prob_MH_1st_part = y_given_beta_delta_optim(delta_proposed) / y_given_beta_delta_optim(delta_current)
+prob_MH_2nd_part = density_delta_current / density_delta_proposed
+prob_MH = min(1, prob_MH_1st_part * prob_MH_2nd_part)
+
+u = runif(1,0,1)  # to draw an independent sample 
+
+if(u <= prob_MH)
+{
+  delta_current = delta_proposed
+}
+if(u > prob_MH)
+{
+  delta_current = delta_current
+}
+
