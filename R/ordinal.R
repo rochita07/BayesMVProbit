ea43372e0fd4fd33cbbf4718e79a8b45f23570e6
Rochita <rochita.das.stat@gmail.com>
2019-12-02 05:46:06 -0600

    Generation of ordinal data

---
+++ R/ordinal.R
@@ -0,0 +1,30 @@
+## Univariate Ordinal
+# Data generation 
+
+cat = 4 ## no of categories
+nu_tot = cat + 1 ## no of cut points 
+nu = nu_tot - 3 ## no of cutpoints to be considered ( nu0, nu1, nuJ are fixed)
+nu_cutoff = c(0, 1.5, 2.5)  # of length nu
+df_t = 5 ## df of proposal t distn for nu
+k = 4
+n = 50
+beta_act = rep(2, k)
+set.seed(1234)
+x = matrix(rnorm(k* n), nrow = k)
+z = beta_act %*% x 
+cutoff = c(-Inf, nu_cutoff, Inf)
+y = rep(0, n) ## initialization
+iter = 5000 ## no of iteration
+burn = 1000 ## no of burn in
+
+for(i in 1:n)
+{
+  for(j in 1: length(cutoff))
+  {
+    if(z[i] > cutoff[j] && z[i] <= cutoff[j + 1]) # making one side inclusive
+      y[i] = j
+  }
+}
+y
+z
+table(y)
