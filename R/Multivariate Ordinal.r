1e92ec60d59630034ed808ef5d10baf518ec0950
Rochita <rochita.das.stat@gmail.com>
2019-12-03 02:41:11 -0600

    plotting of trace plots

--- R/Multivariate Ordinal.r
+++ R/Multivariate Ordinal.r
@@ -314,3 +314,12 @@ beta_mat[iter,]
 Betaout = beta_mat[-c(1:burn), ]
 postmean_beta = colMeans(Betaout)
 rbind(postmean_beta,beta_act) # comparison with actual given betas
+
+# plotting of trace plots
+
+par(mfrow = c(3,4))
+
+for(i in 1 : length(Betaout))
+{
+  traceplot(as.mcmc(Betaout[,i]))
+}
