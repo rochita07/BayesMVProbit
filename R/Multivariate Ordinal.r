972bcb3f424a0c514f0f50980dbf8de3546fcca4
Rochita <rochita.das.stat@gmail.com>
2019-12-03 02:16:02 -0600

    GEneration of X matrix

--- R/Multivariate Ordinal.r
+++ R/Multivariate Ordinal.r
@@ -12,3 +12,29 @@ k = c(2, 3, 2)  ## no of covariates for each variable
 n = 50 # no of subjects
 iter = 5000  # no of iteration
 burn = 1000 # no of burn in 
+
+## to generate the data
+
+beta_act1 = rep(2, k[1])
+beta_act2 = rep(3,k[2])
+beta_act3 = rep(1, k[3])
+beta_act = c(beta_act1, beta_act2, beta_act3)
+beta_dim = sum(k)
+x1 = matrix(rnorm(k[1]* n), nrow = k[1])  # each column is for each subject
+x2 = matrix(rnorm(k[2]* n), nrow = k[2])  # each column is for each subject
+x3 = matrix(rnorm(k[3]* n), nrow = k[3])
+x_mat = list(x1, x2, x3)
+
+#Generation of Xi matrix as list
+x = lapply(1:n, function(x) matrix(rep(0, q * beta_dim), nrow = q) ) ## initialization
+a = matrix(rep(0, q * beta_dim), nrow = q)
+col_indi = c(0, cumsum(k))  # of length q+1  # index of starting and ending points for column in Xi matrix 
+for(i in 1:n)
+{
+  for(l in 1: q)
+  {
+    a[l, (col_indi[l] + 1) : col_indi[l + 1]] = t(x_mat[[l]][,i])
+  }
+  x[[i]] = a
+}
+x
